06971bf3a16dcef5abb9fbf8d442dd34272cdd30
Fernando Torres <fertorresmx@gmail.com>
2018-04-04 02:28:51 -0500

    Eventos y formularios en html y javascript

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,39 +1,41 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener( "click", dibujoPorClick );
+
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-
-var lineas = 30;
-
-var l = 0;
-var yi, xf;
-
-var colorcito = "#FAA";
-
-/*for(l = 0; l < lineas; l++)*/
-do
-{
-	yi = 10 * l;
-	xf = 10 * (l + 1);
-
-	dibujarLinea(colorcito, 0, yi, xf, 300);
-
-	//Esta única línea es la que crea la otra gráfica pero en verde:
-	dibujarLinea("green", 300, 300-yi, 300-xf, 0);
-
-	l++;
-} 
-while (l < lineas)
-
-dibujarLinea(colorcito, 1, 1, 1, 300);
-dibujarLinea(colorcito, 1, 299, 299, 299);
 
 function dibujarLinea (color, x_inicial, y_inicial, x_final, y_final)
 {
 	lienzo.beginPath();
 		lienzo.strokeStyle = color;
-		/*lienzo.lineWidth = 10*/
 		lienzo.moveTo(x_inicial, y_inicial);
 		lienzo.lineTo(x_final, y_final);
 		lienzo.stroke();
 	lienzo.closePath();
+}
+
+function dibujoPorClick()
+{
+
+	var lineas = parseInt( texto.value );
+	var l = 0;
+	var yi, xf;
+	var colorcito = "#FAA";
+	var espacio = ancho / lineas;
+
+	for(l = 0; l < lineas; l++)
+	{
+		yi = espacio * l;
+		xf = espacio * (l + 1);
+		dibujarLinea(colorcito, 0, yi, xf, ancho);
+		//Esta única línea es la que crea la otra gráfica pero en verde:
+		dibujarLinea("green", ancho, ancho-yi, ancho-xf, 0);
+		console.log("Línea: " + l);
+	} 
+
+	dibujarLinea(colorcito, 1, 1, 1, ancho - 1);
+	dibujarLinea(colorcito, 1, ancho - 1, ancho - 1, ancho - 1);
 }
 
