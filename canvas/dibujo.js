52145b797ad35d558c158064d3963d13f812d115
Fernando Torres <fertorresmx@gmail.com>
2018-04-04 02:25:48 -0500

    Solución al reto. Segunda gràfica en otra esquina

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,20 +1,27 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+
 var lineas = 30;
+
 var l = 0;
 var yi, xf;
+
 var colorcito = "#FAA";
 
-/*console.log(lienzo);*/
-
-while(l < lineas)
+/*for(l = 0; l < lineas; l++)*/
+do
 {
 	yi = 10 * l;
 	xf = 10 * (l + 1);
+
 	dibujarLinea(colorcito, 0, yi, xf, 300);
-	console.log("Línea " + l);
+
+	//Esta única línea es la que crea la otra gráfica pero en verde:
+	dibujarLinea("green", 300, 300-yi, 300-xf, 0);
+
 	l++;
-}
+} 
+while (l < lineas)
 
 dibujarLinea(colorcito, 1, 1, 1, 300);
 dibujarLinea(colorcito, 1, 299, 299, 299);
