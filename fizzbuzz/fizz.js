aef3063a0011c1830c7633e792439721c7bf668d
Fernando Torres <fertorresmx@gmail.com>
2018-04-06 21:50:54 -0500

    Reto FizzBuzz con función que revisa divisibilidad

--- fizzbuzz/fizz.js
+++ fizzbuzz/fizz.js
@@ -1,24 +1,31 @@
 var numeros = 100;
-var divisible = false;
 
 for(i = 1; i <=100; i++)
 {
-   divisible = false;
-   if (i % 3 == 0)
+   if (esDivisible(i, 3))
    {
       document.write("Fizz");
-      divisible = true;
    }
 
-   if (i % 5 == 0)
+   if (esDivisible(i, 5))
    {
       document.write("Buzz");
-      divisible = true;
    }
 
-   if (i % 3 != 0 && i % 5 != 0)
+   if (!esDivisible(i, 3) && !esDivisible(i, 5))
    {
       document.write(i);
    }
    document.write("<br />")
+}
+
+function esDivisible(num, divisor)
+{
+   if (num % divisor == 0)
+   {
+      return true;
+   }
+   else {
+      return false;
+   }
 }
